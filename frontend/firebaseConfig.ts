4ff5c92a72d61a2a383204135b31e21a3829eb3b
outoftheboxdev1 <130626635+outoftheboxdev1@users.noreply.github.com>
2024-08-02 19:54:36 -0400

    Changes

--- frontend/firebaseConfig.ts
+++ frontend/firebaseConfig.ts
@@ -3,16 +3,15 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-//! REPLACE VALUES BELOW WITH YOUR OWN FIREBASE CONFIG
 const firebaseConfig = {
-  apiKey: "*****",
-  authDomain: "*****",
-  databaseURL: "*****",
-  projectId: "*****",
-  storageBucket: "*****",
-  messagingSenderId: "*****",
-  appId: "*****",
-  measurementId: "*****",
+  apiKey: "AIzaSyACO3YrtzAqW8L5OHi4DwavH3-FhlaVMe8",
+  authDomain: "cliktr-c562c.firebaseapp.com",
+  databaseURL: "https://cliktr-c562c-default-rtdb.firebaseio.com",
+  projectId: "cliktr-c562c",
+  storageBucket: "cliktr-c562c.appspot.com",
+  messagingSenderId: "188853529319",
+  appId: "1:188853529319:web:601444e3a201066eec9042",
+  measurementId: "G-J6XJ0NC7Z7"
 };
 
 export const FIREBASE_APP = initializeApp(firebaseConfig);
